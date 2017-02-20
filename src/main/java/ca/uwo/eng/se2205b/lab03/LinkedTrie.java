2f27d056737f69c259a1d6b6b1df6050fc5afd58
JedraPeake <jpeake2@uwo.ca>
2017-02-20 18:12:58 -0500

    Question 2 getnextn needs work

--- src/main/java/ca/uwo/eng/se2205b/lab03/LinkedTrie.java
+++ src/main/java/ca/uwo/eng/se2205b/lab03/LinkedTrie.java
@@ -1,8 +1,10 @@
 package ca.uwo.eng.se2205b.lab03;
 
 import javax.annotation.Nonnull;
-import java.util.Map;
+import java.util.HashMap;
+import java.util.Objects;
 import java.util.SortedSet;
+import java.util.TreeSet;
 
 /**
  * Implement a Trie via linking Nodes.
@@ -10,14 +12,21 @@ import java.util.SortedSet;
 public class LinkedTrie implements Trie{
 
     private int size;
-    private Map<SortedSet, String > temp;
+    private TrieNode root;
 
-    public class TrieNode{
+    static class TrieNode{
+        char c;
+        boolean isleaf;
+        HashMap<Character, TrieNode> children = new HashMap<>();
+        public TrieNode() {}
+        public TrieNode(char c){
+            this.c = c;
+        }
 
     }
 
     public LinkedTrie() {
-
+        this.root = new TrieNode();
     }
 
     @Override
@@ -32,21 +41,74 @@ public class LinkedTrie implements Trie{
 
     @Override
     public boolean put(String word) {
-        return false;
+        word = word.toLowerCase();
+        HashMap<Character, TrieNode> temp = this.root.children;
+        for (int i = 0; i<word.length(); i++){
+            TrieNode t;
+            Character check = word.charAt(i);
+            if( temp.containsKey(check)){
+                t = temp.get(check);
+            }
+            else{
+                t = new TrieNode(check);
+                temp.put(check, t);
+            }
+
+            temp = t.children;
+
+            if(i == word.length() -1){
+                t.isleaf = true;
+            }
+        }
+        size++;
+        return true;
     }
 
     @Override
     public int putAll(SortedSet<? extends String> words) {
-        return 0;
+        int count = 0;
+        for (String s : words) {
+            put(s);
+            count ++;
+        }
+        return count;
     }
 
     @Override
     public SortedSet<String> getNextN(@Nonnull String prefix, int N) throws IllegalArgumentException {
-        return null;
+        if( prefix == null || Objects.equals(prefix, "")){
+            throw new IllegalArgumentException();
+        }
+        SortedSet<String> temp = new TreeSet<>();
+        HashMap<Character, TrieNode> tree = this.root.children;
+        TrieNode t = null;
+        for( int i =0; i< N ; i++){
+
+
+
+
+        }
+        return temp;
     }
 
     @Override
     public boolean contains(@Nonnull String word) throws IllegalArgumentException {
-        return false;
+        if(word == null || Objects.equals(word, "")){
+            throw new IllegalArgumentException();
+        }
+        word = word.toLowerCase();
+        HashMap<Character, TrieNode> temp = this.root.children;
+        TrieNode t = null;
+        for( int i =0; i<word.length(); i++){
+            Character check = word.charAt(i);
+            if( temp.containsKey(check)){
+                t = temp.get(check);
+                temp = t.children;
+            }
+            else{
+                return false;
+            }
+        }
+        return t != null && t.isleaf;
     }
 }
--- src/test/java/ca/uwo/eng/se2205b/lab03/TrieTest.java
+++ src/test/java/ca/uwo/eng/se2205b/lab03/TrieTest.java
@@ -2,6 +2,10 @@ package ca.uwo.eng.se2205b.lab03;
 
 import org.junit.Test;
 
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  *  Tests for Trie implementation
  */
@@ -11,16 +15,59 @@ public class TrieTest {
 
     @Test
     public void sizeAndIsEmpty() throws Exception {
+        assertEquals(0, underTest.size() );
+        assertEquals(true, underTest.isEmpty());
+
+        underTest.put("word");
+        assertEquals(true, underTest.contains("word") );
+        assertEquals(1, underTest.size() );
+        assertEquals(false, underTest.isEmpty());
+
         // Check empty tree, after adding and removing elements
     }
 
     @Test
     public void put() throws Exception {
+        underTest.put("word");
+        assertEquals(true, underTest.contains("word") );
+        assertEquals(1, underTest.size() );
+        assertEquals(false, underTest.isEmpty());
+
+        underTest.put("do");
+        assertEquals(true, underTest.contains("do") );
+        assertEquals(2, underTest.size() );
+        assertEquals(false, underTest.isEmpty());
+
         // Check what happens when adding and contains
     }
 
     @Test
     public void putAll() throws Exception {
+//        SortedSet<String> temp = new SortedSet<String>();
+        SortedSet<String> temp = new TreeSet<>();
+        assertEquals(0, underTest.putAll(temp));
+
+        temp.add("Hello");
+        assertEquals(1, underTest.putAll(temp));
+        assertEquals(true, underTest.contains("Hello"));
+
+        // make sure it works compared to put
+    }
+
+    @Test
+    public void putAllTest2() throws Exception {
+        SortedSet<String> temp = new TreeSet<>();
+
+        temp.add("Hello");
+        temp.add("Hi");
+        temp.add("Z");
+        assertEquals(3, underTest.putAll(temp));
+        assertEquals(true, underTest.contains("Hello"));
+        assertEquals(true, underTest.contains("Hi"));
+        assertEquals(true, underTest.contains("hi"));
+        assertEquals(true, underTest.contains("Z"));
+        assertEquals(false, underTest.contains("c"));
+
 
         // make sure it works compared to put
     }
