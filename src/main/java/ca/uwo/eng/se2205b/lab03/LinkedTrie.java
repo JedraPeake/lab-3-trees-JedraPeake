9a0d1ce2eb7d08a9846493fba74abbc3ae5d9e50
JedraPeake <jpeake2@uwo.ca>
2017-03-01 13:47:29 -0500

    load complete left

--- src/main/java/ca/uwo/eng/se2205b/lab03/LinkedTrie.java
+++ src/main/java/ca/uwo/eng/se2205b/lab03/LinkedTrie.java
@@ -12,42 +12,125 @@ public class LinkedTrie implements Trie{
     private TrieNode root;
 
     @Override
+//    public SortedSet<String> getNextN(@Nonnull String prefix, int N) throws IllegalArgumentException {
+//        if( prefix == null || Objects.equals(prefix, "")){
+//            throw new IllegalArgumentException();
+//        }
+//        SortedSet<String> temp = new TreeSet<>();
+//        HashMap<Character, TrieNode> tree = this.root.children;
+//        prefix = prefix.toLowerCase();
+//        TrieNode t = null;
+//        String s="";
+//        for( int i =0; i< N ; i++){
+//            Character check = prefix.charAt(i);
+//            if( tree.containsKey(check)){
+//                s +=check;
+//                t = tree.get(check);
+//            }
+//            else{
+//                s = "";
+//            }
+//            if(Objects.equals(s, prefix)){
+//                temp.add(s);
+//            }
+//        }
+//        return temp;
+//    }
     public SortedSet<String> getNextN(@Nonnull String prefix, int N) throws IllegalArgumentException {
         if( prefix == null || Objects.equals(prefix, "")){
             throw new IllegalArgumentException();
         }
-        SortedSet<String> temp = new TreeSet<>();
-        HashMap<Character, TrieNode> tree = this.root.children;
-        prefix = prefix.toLowerCase();
-        TrieNode t = null;
-        String s="";
-        for( int i =0; i< N ; i++){
-            Character check = prefix.charAt(i);
-            if( tree.containsKey(check)){
-                s +=check;
-                t = tree.get(check);
+        //Trying to find the stem in Trie
+        String prefixToCheckLowerCase = prefix.toLowerCase();
+        int completionsCount = 0;
+        SortedSet<String> completions = new TreeSet<>();
+        TrieNode traversal = root;
+        String t ="";
+        String s=prefixToCheckLowerCase;
+        for (int i = 0; i < prefixToCheckLowerCase.length(); i++)
+        {
+            if (traversal.getValidNextCharacters().contains(prefixToCheckLowerCase.charAt(i)))
+            {
+                traversal = traversal.getChild(prefixToCheckLowerCase.charAt(i));
+                t+= prefixToCheckLowerCase.charAt(i);
             }
-            else{
-                s = "";
-            }
-            if(Objects.equals(s, prefix)){
-                temp.add(s);
+            //returns an empty list
+            else {
+                return completions;
             }
         }
-        return temp;
-    }
+        //If current word is an end word, increment the counter and add it to completions list
+        if (traversal.endsWord())
+        {
+            completionsCount=1;
+            completions.add(t);
+            t="";
+        }
 
+        List<TrieNode> nodesToBeSearched = new LinkedList<TrieNode>();
+        List<Character> ChildCharaterList = new LinkedList<Character>(traversal.getValidNextCharacters());
+        //Filling the list with children of the current node, first level of of the breadth first search
+        for (int i=0; i<ChildCharaterList.size(); i++)
+        {
+            nodesToBeSearched.add(traversal.getChild(ChildCharaterList.get(i)));
+            t+=ChildCharaterList.get(i);
+        }
+        //while loop for the linked list elements and see if any completions exists , inside it we will also check each node children and add them to the list!!!
+        while (nodesToBeSearched!=null  && nodesToBeSearched.size()>0 && completionsCount < N)
+        {
+            TrieNode trieNode = nodesToBeSearched.remove(0);
+            if (trieNode.endsWord())
+            {
+                s += t;
+                completionsCount++;
+                completions.add(s);
+                t="";
+            }
+
+            List<Character> subTrieNodeCholdren = new LinkedList<Character>(trieNode.getValidNextCharacters());
+            //Adding all next level tries to the linked list , kinda recursive!!!
+            for (int i=0; i<subTrieNodeCholdren.size();i++)
+            {
+                nodesToBeSearched.add(trieNode.getChild(subTrieNodeCholdren.get(i)));
+                t+=subTrieNodeCholdren.get(i);
+            }
+        }
+        return completions;
+    }
 
     static class TrieNode{
         char c;
+        boolean isWord;
         boolean isleaf;
         HashMap<Character, TrieNode> children = new HashMap<>();
         public TrieNode() {}
         public TrieNode(char c){
+            this();
             this.c = c;
         }
         public char get(){
             return this.c;
+        }
+        private Set<Character> getValidNextCharacters()
+        {
+            return children.keySet();
+        }
+        private TrieNode getChild(Character c)
+        {
+            return children.get(c);
+        }
+        private boolean endsWord()
+        {
+            return isWord;
+        }
+        private void setEndsWord(boolean b)
+        {
+            isWord = b;
+        }
+        public String getText()
+        {
+            String text = String.valueOf(c);
+            return text;
         }
 
     }
@@ -85,6 +168,7 @@ public class LinkedTrie implements Trie{
 
             if(i == word.length() -1){
                 t.isleaf = true;
+                t.isWord = true;
             }
         }
         size++;
